49d2dd11830ca9711a34541f88e735e6bf79ddc0
Joel Piper <what_the_hells_an_@hotmail.com>
2015-03-26 18:36:03 +0000

    Solution for project use
    Added Visual Studio solution for use with the project, doxygen is
    include for use with the documentation

---
+++ SEP-GroupAssignment-Project/SEP-GroupAssignment-Project/main.cpp
@@ -0,0 +1,16 @@
+/*! \mainpage Documentation for The Software Engineering Group Project
+* \section intro_sec Introduction 
+* 
+* This is the documentation for the 2D game project created for Dr Glenn L Jenkins'
+* Software Engineering Principles Group Work Assignment
+*
+* \section name_sec Contributors
+*
+* Joel Ross Piper - P129196
+* Craig Damien Davies
+* David Burdett
+*
+*
+*
+*
+*/
