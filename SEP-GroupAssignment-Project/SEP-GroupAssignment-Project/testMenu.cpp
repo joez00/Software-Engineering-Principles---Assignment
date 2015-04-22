b99c319f5172b0a31ff8742907dc10dc897dd820
Joel Piper <what_the_hells_an_@hotmail.com>
2015-04-22 20:40:55 +0000

    Added Menu and testMenu classes
    Added Menu Class and test class for menu class

---
+++ SEP-GroupAssignment-Project/SEP-GroupAssignment-Project/testMenu.cpp
@@ -0,0 +1,11 @@
+#include "testMenu.h"
+
+
+testMenu::testMenu(void)
+{
+}
+
+
+testMenu::~testMenu(void)
+{
+}
---
+++ SEP-GroupAssignment-Project/SEP-GroupAssignment-Project/testMenu.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Menu.h"
+
+class testMenu
+{
+public:
+	testMenu(void);
+	~testMenu(void);
+};
+
